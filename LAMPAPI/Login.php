1f573a57790acfb1967c908627d2287b7c4acf8e
Gavin Tryzbiak <gavintryz@gmail.com>
2021-01-29 19:40:16 -0500

    Fixed MySQL info

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -6,7 +6,7 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "root", "123NotPassword", "MASTERCOP4331");
+	$conn = new mysqli("localhost", "root", "mysqlpass", "MASTERCOP4331");
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
