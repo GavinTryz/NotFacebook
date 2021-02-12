979605f7662a04f1cb501536b62672ad95c5937c
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-02-11 22:12:12 -0500

    Revert "Merge branch 'api' of https://github.com/GavinTryz/NotFacebook into api"
    This reverts commit fd9d55b3916907d6165e521e217f35f6fcc1b0c2, reversing
    changes made to 038ea020a08e8df6fbbae8e87c3e5b61f04b00b3.
    
    # Conflicts:
    #	LAMPAPI/AddUser.php
    #	LAMPAPI/SearchContact.php

--- LAMPAPI/AddUser.php
+++ LAMPAPI/AddUser.php
@@ -1,43 +1,41 @@
 <?php
+/**
+ * @var $conn
+ */
+
+    include('config/db_connect.php');
     $inData = getRequestInfo();
 
-    // Create account fields
-    $newUsername = $inData["newUsername"];
-    $newPassword = $inData["newPassword"];
-    $newPasswordConf = $inData["newPasswordConf"];
+    //create account fields
+    $username = $inData["username"];
+    $password = $inData["password"];
 
-    $conn = new mysqli("localhost", "API", "123NotPassword", "MASTER");
-    if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	}
+    //check if username already exists
+    if ($conn->connect_error)
+    {
+        returnWithError( $conn->connect_error );
+    }
     else
     {
-        // todo Check for duplicate
-        // todo Confirm password
-        $sql = "insert into USERS(USERNAME, PASSWORD) values ('$newUsername','$newPassword')";
-        //$sql = "insert into USERS (USERNAME, PASSWORD) values ('" . $newUsername . "', '" . $newPassword . "')";
-        if($result = $conn->query($sql) != TRUE)
-        {
-            returnWithError($conn->error);
+        //SQL SEARCH STRING
+        $sql_u = "SELECT * FROM USERS WHERE username='$username'";
+        if(mysqli_query($conn, $sql_u) > 0){
+            //todo GAVIN UR STRING
+            $username_error = "Username already taken.";
+        }else {
+            //add contact
+            $sql_u_add = "INSERT INTO USERS(username, password) 
+                VALUES('$username','$password')";
+            //save to DB
+            if (mysqli_query($conn, $sql_u_add)) {
+                //close connection to the DB
+                mysqli_close($conn);
+                //if successful, redirect user to home page.
+                header('Location: index.html');
+            } else {
+                $sql_error =  'Query Error: ' . mysqli_error($conn);
+            }
         }
         $conn->close();
     }
-    returnWithError(""); // Return with empty error, to signal account creation successful
 
-    function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-    function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError($err)
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson($retValue);
-	}
