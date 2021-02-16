a7e722b57574cb78fe4811e36c472c371d81c6e3
MatheusF1010 <74433879+MatheusF1010@users.noreply.github.com>
2021-02-16 17:45:32 -0500

    Update code.js
    updates page

--- js/code.js
+++ js/code.js
@@ -559,6 +559,7 @@ function deleteContact(row)
   		{
   			if (this.readyState == 4 && this.status == 200) 
   			{
+				searchAccount();
   				//document.getElementById("contactDeleteResult").innerHTML = "Contact has been Deleted";
   			}
   		};
